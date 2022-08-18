d73a801283db69ef8a6673c344c6ffe610f95159
Sean <abbb123p@gmail.com>
2022-08-19 01:45:57 +0800

    change words to chinese in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: "home#index"
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
