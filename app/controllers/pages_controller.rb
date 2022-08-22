c7185a1c82ae615ad6a10654361ce3c5bb21c46a
ker ker <kerke.2011@gmail.com>
2022-08-22 23:21:41 +0800

    add index.html.erb skeleton

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  layout "index"
   def home; end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  content: [
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*",
+  ],
   theme: {
     extend: {},
   },
