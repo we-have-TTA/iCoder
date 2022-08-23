9b6c3fb68639f38d25a9c9dfd9efc54ea75ba1a8
Sean <abbb123p@gmail.com>
2022-08-23 15:33:46 +0800

    apply fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # member
   devise_for :users
+  # 解決devise註冊及忘記密碼出錯時重新整理沒路徑
+  devise_scope :user do
+  get '/users', to: 'devise/registrations#new'
+  get '/users/password', to: 'devise/passwords#new'
+  end
+
   get 'pages/home'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
