48977ee73675671073fd7ab3f262528eb1eaa5f1
tienyuan <chutienyuan@gmail.com>
2022-08-27 16:11:09 +0800

    feat:Add room controller
    新增room controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,36 @@
+class RoomsController < ApplicationController
+    def index
+        @rooms = Room.all
+    end
+
+    def show
+        @room = Room.find_by(id: params[:id]) 
+    end
+
+    def new
+        @room = Room.new
+    end
+
+    def create
+        @room = current_user.rooms.new(rooms_params)
+        if @room.save
+            redirect_to rooms_path
+        else
+            render :new
+        end
+    end
+
+
+    def destroy
+        @room = Rook.find_by(id: params[:id])
+        @room.destroy
+        redirect_to rooms_path
+    end
+
+
+    private
+    def rooms_params
+        params.require(:room).permit(:title, :language)
+    end 
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
 
   get 'pages/home'
   root to: 'pages#home'
+
+  scope "dashboard" do
+    resources :rooms
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
