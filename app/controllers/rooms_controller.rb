28f0d2d6dc29cb31a0cd9e72e7a4966c7dc3187e
tienyuan <chutienyuan@gmail.com>
2022-08-28 18:18:41 +0800

    wip:修改關聯

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,7 +10,18 @@ class RoomsController < ApplicationController
   end
 
   def new
-    @room = Room.new
+    title = "Untitled Room - " + SecureRandom.alphanumeric(6).upcase
+    status = "Not Started"
+    category = "Live"
+    language = "JavaScript"
+    users_id = current_user.id
+    teams_id = current_user.team_id
+    room = Room.new(title: title, status: status, category: category, language: language,
+                    users_id: users_id,
+                    teams_id: teams_id
+                    )
+    room.save
+    redirect_to room_path(id: room.id)
   end
 
   def create
@@ -31,6 +42,6 @@ class RoomsController < ApplicationController
   private
 
   def rooms_params
-    params.require(:room).permit(:title, :language, :category, :status)
+    params.require(:room).permit(:title, :language, :category, :status).merge(team_id:current_user.team_id)
   end
 end
