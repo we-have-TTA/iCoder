b9b68ffbfa2cf238b59772f8b57838d6a55ecc11
ker ker <kerke.2011@gmail.com>
2022-08-29 21:35:47 +0800

    create new team if user does not get invitation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_093419) do
+ActiveRecord::Schema.define(version: 2022_08_29_133401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 2022_08_29_093419) do
     t.string "plan", default: "normal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
