31c21b11ba65568cea3cdf637cd04a213f4e195a
yojijun <vivians_9800@yahoo.com.tw>
2022-09-01 02:35:22 +0800

    WIP Question->create Q.page 3-1
    題目新增三步驟 步驟一ing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,21 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-def index
-  
-end
+  def index
+    # @question = question.all
+  end
 
-def new
-end
+  def new
+    # @question = question.new
+  end
 
-def create
-end
+  def create; end
 
-def show
-end
+  def show; end
 
-def edit
-end
+  def edit; end
 
-def update
-end
+  def update; end
 
-def destory
-end
-  
+  def destory; end
 end
