d848b90716434f1503668682f73f6c0fd875285c
yojijun <vivians_9800@yahoo.com.tw>
2022-09-02 17:24:40 +0800

    Questions CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
+    # render html: params
   end
 
   def new
@@ -9,25 +10,55 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    # find_team
     @question = current_user.questions.new(clean_params)
     if @question.save
-      redirect_to "/questions",notice: "question created"
+      redirect_to "/dashboard/questions",notice: "question created"
     else
       render :new
     end
   end
 
-  def show; end
+  def show
+    find_question
+  end
 
-  def edit; end
+  def edit
+    find_question
+  end
 
-  def update; end
+  def update
+    find_question
+    if @question.update(clean_params)
+      redirect_to "/dashboard/questions",notice: "Edited!!"
+    else
+      render :edit
+    end
+  end
 
-  def destory; end
+  def destroy
+    find_question
+    @question.destroy
+    redirect_to "/dashboard/questions",notice: "DELETE!!"
+  end
+
+  
+  def search
+    render html: params
+  end
+  
   private
+
+  def find_question
+    @question =Question.find(params[:id])
+    # @question = current_user.team 
+  end
+  
+  def find_team
+    @team = current_user.team 
+  end
+
   def clean_params
-    params.require(:question).permit(:title, :code)
+    params.require(:question).permit(:title, :code).merge(team: current_user.team)
   end
 end
 
