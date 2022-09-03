26f62b1e9e1bfd97bac2d06fd9f2dacd3f10241e
yojijun <vivians_9800@yahoo.com.tw>
2022-09-04 00:30:44 +0800

    change column difficulty in questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_03_093632) do
+ActiveRecord::Schema.define(version: 2022_09_03_162556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2022_09_03_093632) do
     t.text "code"
     t.text "internal_description"
     t.text "candidate_instructions"
-    t.text "difficulty"
+    t.string "difficulty"
     t.index ["team_id"], name: "index_questions_on_team_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
