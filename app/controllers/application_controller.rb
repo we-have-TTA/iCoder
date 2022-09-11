5c44014e608d6e68d5f355b92590c8cfa217278c
ker ker <kerke.2011@gmail.com>
2022-09-11 14:47:22 +0800

    fix app/application_controller.rb, move method to private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
   def after_sign_in_path_for(_resource)
     rooms_path
   end
-
-  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
