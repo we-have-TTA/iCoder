d3a7ff8d65f5f322de32001c528b048bb6fc7d63
RexKao <ww10282002@gmail.com>
2022-09-12 12:06:53 +0800

    python and elixir can connect

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -6,7 +6,7 @@ import hljs from "highlight.js"
 import { withLineNumbers } from "codejar/linenumbers"
 // Connects to data-controller="editor"
 export default class extends Controller {
-  static targets = ["panel", "draw", "change_language"]
+  static targets = ["panel", "draw", "change_language", "team_name", "questions_list", "questions_information"]
   connect() {
     // 目前編輯器語言
     this.panelTarget.className += " ruby"
@@ -18,7 +18,7 @@ export default class extends Controller {
     const str = `puts 123`
     jar.updateCode(str)
 
-    this.toggleLanguageMenu()
+    // this.toggleLanguageMenu()
   }
 
   run() {
@@ -62,20 +62,83 @@ export default class extends Controller {
       : this.change_languageTarget.classList.add("hidden")
   }
 
-  catchQuestions() {
-    const roomID = this.element.dataset.room_id
-    Rails.ajax({
-      url: `/api/v1/rooms/${roomID}/catch_questions`,
-      type: "get",
-      success: (result) => {
-        console.log(result)
-        result.forEach((question) => {
-          console.log(question.title)
-        })
-      },
-      error: (err) => {
-        console.log(err)
-      },
-    })
-  }
+  // catchQuestions(e) {
+  //   const roomID = this.element.dataset.room_id
+  //   Rails.ajax({
+  //     url: `/api/v1/rooms/${roomID}/catch_questions`,
+  //     type: "get",
+  //     success: (result) => {
+  //       // console.log(result.question)
+  //       // console.log(result.team)
+  //       console.log(e)
+  //       let html = ""
+  //       this.team_nameTarget.textContent=`${result.team.name} questions`
+  //       result.question.forEach((question) => {
+  //         html += `<tr>
+  //                    <td data-action="click->editor#catchQuestions"
+  //                        data-editor-target="question_information"
+  //                        id=${question.id}>
+  //                      <div class="text-left font-bold">
+  //                        ${question.title}
+  //                      </div>
+  //                      <div class="text-left">
+  //                        ${question.internal_description}
+  //                      </div>
+  //                      <div class="text-left">
+  //                        ${question.language}・By ${result.user[question.user_id-1].username}
+  //                      </div>
+  //                    </td>
+  //                  </tr>`
+          
+          
+  //       })
+  //       // document.getElementById("list").addEventListener("click", (e)=> {
+  //       //   console.log(e.currentTarget)
+  //       // })
+  //       this.questions_listTarget.innerHTML = ""
+  //       this.questions_listTarget.insertAdjacentHTML("afterbegin", html)
+  //       let informationHtml = ""
+  //       console.log(this)
+        
+  //       informationHtml = `<div class=" border">
+  //                            <div class=" font-bold px-5 py-3">
+  //                              ${result.question[0].title}
+  //                            </div>
+  //                            <div class=" px-5">
+  //                              ${result.question[0].internal_description
+  //                              }
+  //                            </div>
+  //                            <div class=" px-5 pb-3">
+  //                              ${result.question[0].created_at}
+  //                            </div>
+  //                          </div>
+  //                          <div class=" border-l border-r">
+  //         <span class=" pl-5 pt-1 inline-block"><span class="border-b py-2 inline-block hover:border-gray-900">給面試者的訊息</span></span>
+  //         <span class=" pl-5 pt-1 inline-block"><span class="border-b py-2 inline-block hover:border-gray-900">Code</span></span>
+  //       </div>
+  //       <div class="px-5 py-8">
+  //         ${result.question[0].code}
+  //       </div>`
+        
+  //       // this.questions_informationTarget.innerHTML = ""
+  //       // this.questions_informationTarget.insertAdjacentHTML("afterbegin", informationHtml)
+  //       // // console.log(this)
+  //       // this.panelTarget.className += " ruby"
+  //       // const jar = CodeJar(
+  //       //   this.panelTarget,
+  //       //   hljs.highlightElement
+  //       // )
+  //       // const str=`${result.question[0].code}`
+  //       // jar.updateCode(str)
+    
+
+  //       // console.log(this.connect().CodeJar)
+        
+
+  //     },
+  //     error: (err) => {
+  //       console.log(err)
+  //     },
+  //   })
+  // }
 }
