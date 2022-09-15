8ee3cce37cd59521d607ded7dd72fbc5d1f54ebd
Carry <zxcvbnm75480@gmail.com>
2022-09-15 21:00:30 +0800

    roomtitle可隨意修改,且存入資料庫

--- app/javascript/controllers/roomtitle_controller.js
+++ app/javascript/controllers/roomtitle_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = ["certain", "nocertain", "title", "input"]
@@ -11,7 +12,18 @@ export default class extends Controller {
   revise_roomtitle() {
     const input = this.inputTarget.value
     this.element.innerHTML = `<button data-action="click->roomtitle#changeTitle"><img src="https://upload.wikimedia.org/wikipedia/commons/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg"class="h-7 w-7"></button>
-    <span data-roomtitle-target="title">${input}</span>`
+    <span data-roomtitle-target="title" data-room_change="input">${input}</span>`
+    const id = this.element.dataset.room_id
+    const data = { id: id, input: input }
+    Rails.ajax({
+      url: "/api/v1/rooms/change_roomtitle",
+      type: "patch",
+      data: new URLSearchParams(data).toString(),
+      success: (res) => {
+        console.log(res)
+      },
+      error: () => {},
+    })
   }
 
   no_revise_roomtitle() {
