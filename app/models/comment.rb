431a6cca975ddec988b1f23e30439003192c9c05
Sean <abbb123p@gmail.com>
2022-09-15 14:56:23 +0800

    remove action text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :comments, foreign_key: :parent_id, dependent: :destroy
-  # has_rich_text :content
   validates :content, presence: true
 end
