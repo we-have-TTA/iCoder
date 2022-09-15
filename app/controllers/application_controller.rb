a1740df5738f90308b04003c27bd1c329b0c5e5d
Sean <abbb123p@gmail.com>
2022-09-15 16:27:52 +0800

    move devise sanitize from application to registration controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-
-  def after_sign_in_path_for(_resource)
-    rooms_path
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
+  
 end
