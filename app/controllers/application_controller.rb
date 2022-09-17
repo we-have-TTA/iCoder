20d650a11ed55925a20d4ff6031e626d8aa7c2c9
RexKao <ww10282002@gmail.com>
2022-09-17 15:07:57 +0800

    question創建會議室也增加前端權限阻擋效果

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pagy::Backend
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :no_permission
+
+  private
+
+  def no_permission
+    redirect_to "/", notice: "無此操作權限"
+  end
 end
