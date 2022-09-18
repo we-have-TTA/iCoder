9cb87a3b74c2e210bf80dadff59c66fea06ba28b
yojijun <vivians_9800@yahoo.com.tw>
2022-09-18 11:18:03 +0800

    add last_used column to question

---
+++ db/migrate/20220917173030_add_last_used_column_to_question.rb
@@ -0,0 +1,5 @@
+class AddLastUsedColumnToQuestion < ActiveRecord::Migration[6.1]
+  def change
+    add_column :questions, :last_used, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_095455) do
+ActiveRecord::Schema.define(version: 2022_09_17_173030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_095455) do
     t.text "candidate_instructions"
     t.string "difficulty"
     t.text "question_type"
+    t.string "last_used"
     t.index ["team_id"], name: "index_questions_on_team_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
