d43a5cc9172ef9b994a36920cb49e5102a54d61e
chu <t91127@hotmail.com>
2022-09-20 02:01:55 +0800

    fix team_mailer

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -3,7 +3,6 @@
 class TeamMailer < ApplicationMailer
   def send_invitation_to(user, team_id, register, team_name)
     @team_name = team_name
-    @team_id = team_id
     @user = user
     host_ip = if ENV['RAILS_ENV'] == 'production'
                 ENV.fetch('DOMAIN_NAME', nil)
