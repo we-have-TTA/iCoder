e9fb3c29c58d227893d5c40d65e65ef46c373e01
chu <t91127@hotmail.com>
2022-09-22 11:02:15 +0800

    wip room的篩選

--- app/javascript/controllers/room_controller.js
+++ app/javascript/controllers/room_controller.js
@@ -2,26 +2,30 @@ import { Controller } from "stimulus"
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = ["filterCurrentUserButton", "roomTable","countdown"]
+  static targets = ["filterCurrentUserButton", "roomTable", "countdown"]
 
   connect() {
     Rails.ajax({
       url: "/dashboard/rooms/countdown",
       type: "get",
-      success: ( {rooms_duration} ) => {
+      success: ({ rooms_duration }) => {
         const deleteButton = this.countdownTargets
-        const countdownStandard = (this.element.dataset.countdown_standard*60*60)
-        deleteButton.forEach((target)=> {
+        const countdownStandard =
+          this.element.dataset.countdown_standard * 60 * 60
+        deleteButton.forEach((target) => {
           rooms_duration.forEach((room) => {
             if (target.dataset.room_uuid == room.uuid) {
-              let offsetTime = Number(countdownStandard-room.existTime)
-              setInterval(()=> {
+              let offsetTime = Number(countdownStandard - room.existTime)
+              setInterval(() => {
                 offsetTime -= 1
-                let sec = String(parseInt(offsetTime % 60)).padStart(2,'0')
-                let min = String(parseInt((offsetTime / 60) % 60)).padStart(2,'0')
-                let hr = String(parseInt(offsetTime / 60 / 60)).padStart(2,'0')
+                let sec = String(parseInt(offsetTime % 60)).padStart(2, "0")
+                let min = String(parseInt((offsetTime / 60) % 60)).padStart(
+                  2,
+                  "0"
+                )
+                let hr = String(parseInt(offsetTime / 60 / 60)).padStart(2, "0")
                 target.textContent = `距可刪除\n${hr}:${min}:${sec}`
-              }, 1000);
+              }, 1000)
             }
           })
         })
@@ -47,6 +51,7 @@ export default class extends Controller {
       success: ({ rooms }) => {
         let html = ""
         var regexp = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g
+        console.log(rooms)
         for (var i = 0; i < rooms.length; i++) {
           html += `<tr>
               <td>${rooms[i].title}</td>
