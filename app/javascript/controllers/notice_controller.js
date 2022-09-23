741f1d98674c17b41ae02874f0c1a1c36140cae5
Sean <abbb123p@gmail.com>
2022-09-23 18:12:14 +0800

    fix notice width for all web

--- app/javascript/controllers/notice_controller.js
+++ app/javascript/controllers/notice_controller.js
@@ -2,9 +2,9 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    // setTimeout(() => {
-    // this.dismiss()
-    // }, 5000)
+    setTimeout(() => {
+      this.dismiss()
+    }, 5000)
   }
   dismiss() {
     this.element.remove()
