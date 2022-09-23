e50d7c3e0451d1cd0991ad1a60f6390221bce545
Sean <abbb123p@gmail.com>
2022-09-23 21:17:35 +0800

    remove dependent of question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id', dependent: :destroy
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :team
 
   has_many :comments, dependent: :destroy
